33e495e2385b844b289562966ae15300afb25cf5
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-11-07 12:32:20 -0800

    working on getting  Auth0 set up, added Profile component/feature.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login'
 import Logout from './components/Logout'
+import Profile from './components/Profile'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <h1>VibesVille</h1>
     <Login />
     <Logout />
+    <Profile />
     </>
   );
 }
