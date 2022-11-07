c3cfbc974f1aa66748170e426d8af70714c25829
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-11-07 13:07:58 -0800

    adding notes for next steps

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 import React from "react";
 
 const LoginButton = () => {
@@ -8,3 +9,49 @@ const LoginButton = () => {
 };
 
 export default LoginButton;
+
+
+
+//add this to Savedcity/BestCity component->
+
+// import { useAuth0 } from "@auth0/auth0-react";  <------
+
+// then wrap export default BestCity component with higher order component ->
+
+// export default withAuth0(BestCity);   <---
+
+//----------------------------------------------------------------
+
+class BestCity extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      cities: []
+    }
+  }
+  
+  async componenentDidMount(){
+
+    if(this.props.auth0.isAuthenticated){
+      const res = await this.props.auth0.getIdTokenClaims();
+
+      const jwt = res.__raw;
+
+      console.log('token: ', jwt);   
+
+      const config = {
+        header: ( "Authorization": `Bearer ${jwt}`),
+        method: 'get',
+        baseURL: process.env.REACT_APP_SERVER,
+        url: '/cities'
+      }
+      let cityData = await axios.get(config);
+      
+      
+      this.setState({
+        city: cityData.data
+      })
+    }
+  }
+  
+}
