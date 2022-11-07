3046b77b78a6216fda0ff53ab9dd5aced376a609
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-11-07 12:29:52 -0800

    working on getting  Auth0 set up with login components/features.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login'
+import Logout from './components/Logout'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>VibesVille</h1>
+    <Login />
+    <Logout />
+    </>
   );
 }
 
-export default App;z
+export default App;
