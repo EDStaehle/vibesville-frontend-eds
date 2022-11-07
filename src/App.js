9d5fc1ef5719b00db2b3b40eeb1b7c6ba6aad60a
Seth <sethparkerpierce@icloud.com>
2022-11-07 18:28:51 -0500

    fixed merge errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 import Main from './Main'
+import './App.css';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Profile from './components/Profile';
+
 export default class App extends Component {
   render() {
     return (
-      <div><Main/></div>
+      <div>
+        <h1>VibesVille</h1>
+        <Login />
+        <Logout />
+        <Profile />
+        <Main/>
+      </div>
     )
   }
 }
