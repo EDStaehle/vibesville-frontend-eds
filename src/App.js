42ef41e845c35c679fb138589ba572f841816452
Elias <elias.staehle@gmail.com>
2022-11-07 19:33:06 -0500

    sidebar logic and rendering

--- src/App.js
+++ src/App.js
@@ -1,66 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
-import Logout from './components/Logout';
+// import Login from './components/Login';
+// import Logout from './components/Logout';
 import Profile from './components/Profile';
 // import BestJobs from './components/BestJobs';
 // import { useAuth0 } from "@auth0/auth0-react";
+import data from './data.json'
+import Sidebar from './Sidebar';
 
-<<<<<<< HEAD
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       saved: [],
       stars: '',
+      data: data,
+      show: false
     }
   }
+
+showCanvas = () => {
+  this.setState({
+    show: true
+  })
+}
+hideCanvas = () =>{
+  this.setState({
+    show: false
+  })
+}
+
   render() {
     return (
-      <div>App</div>
+      <>
+        <Sidebar
+        
+          saved={this.state.saved}
+          favs={this.state.stars}
+          data={this.state.data}
+          show={this.state.show}
+         showCanvas={this.showCanvas}
+         hideCanvas={this.hideCanvas}
+        />
+
+
+      </>
     )
   }
 }
 
 
 
-=======
-function App() {
-  return (
-    <>
-    <h1>VibesVille</h1>
-    <Login />
-    <Logout />
-    <Profile />
-    </>
-  );
-}
-
-export default App;
-
-//add <BestJobs />
-// wrap App -->
-// export default withAuth0(App);
-
-
-// function App() {
-//   return (
-//     <>
-//       <h1>VibesVille</h1>
-//       {this.props.auth0.isAuthenticated ?
-//         <>
-//           <Profile />
-//           <BestJobs />
-//           <Logout />
-//         </>
-//         :
-//         <Login />
-//       }
-//     </>
-//   );
-// }
-
-// export default withAut0(App);
->>>>>>> 6ad6c989cd1cadcd86892375ae396d35aeddd257
