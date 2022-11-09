07b3f3f9fc53b751c94cdb1082225f94481d9870
Elias <elias.staehle@gmail.com>
2022-11-08 19:07:35 -0500

    added navbar and route functionality

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { Navbar, NavItem } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Container from 'react-bootstrap/Container';
+import "./Header.css"
+export default class Header extends Component {
+  render() {
+    return (
+      <>
+        {[false].map((expand) => (
+          <Navbar key={expand} bg="light" expand={false} className="mb-3">
+            <Container fluid>
+              <Navbar.Brand>VibesVille</Navbar.Brand>
+              <Navbar.Toggle />
+              <Navbar.Offcanvas
+                placement="start"
+              >
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title >Navigation</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <NavItem><Link to="/" className="nav-link">Home</Link></NavItem>
+                  <NavItem><Link to="/Dashboard" className="nav-link">Dashboard</Link></NavItem>
+                  <NavItem><Link to="/about" className="nav-link">About</Link></NavItem>
+
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </Container>
+          </Navbar>
+        ))}
+      </>
+    )
+  }
+}
