97cdfe0ae7826c28f1e9bb9ae4eb06c4d3b9d255
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-11-09 15:03:39 -0800

    about us page, added About.css file and resized images

--- src/About.js
+++ src/About.js
@@ -36,7 +36,14 @@ import "./About.css";
           <img src='./assets/RC.JPEG' alt='Raphael'/>
           <div className='container'>
             <h2>Raphael Chookagian</h2>
-            <p>bio of team member here. Lorem ipsum ipsum</p>
+            <p> Hello! My name is Raphael Chookagian. I am a student at Code Fellows full-stack JavaScript program. I have been a commercial photographer for the past decade and am now transitioning into web development. <br />
+            Born in Rio de Janeiro, Brazil. U.S. Army combat veteran, certified personal trainer and fashion photographer. Well versed in working internationally within teams and solo to accomplish set objectives. <br />
+            You can find me on
+            <a href="https://www.linkedin.com/in/raphaelchookagian/">
+              Linkedin
+            </a>
+            and <a href="https://github.com/cesarderio"> GitHub</a>. <br />
+            Thank you for checking out our project!</p>
           </div>
         </div>
       </div>
