c1930137e6b7734393afda3cfc9df5bfc6f29b45
Dennis Nichols <dgnichols22@gmail.com>
2022-11-10 17:48:32 -0300

    updated job card score colors

--- src/JobCard.js
+++ src/JobCard.js
@@ -6,6 +6,17 @@ export default class JobCard extends Component {
   openModal = () => {
     this.props.modalOpen(this.props.job);
   };
+  scoreColor = (score) => {
+    if (score < 3) {
+      return 'red';
+    }
+    else if (score < 6) {
+      console.log('did something with score');
+      return 'yellow'; 
+    }
+   
+    else return 'green';
+  }
 
   render() {
     let score = this.props.job.CityData
@@ -53,31 +64,51 @@ export default class JobCard extends Component {
               <div id="cityVibeContainer">
                 {this.props.job.CityData ? (
                   <ListGroup>
-                    <ListGroup.Item>
-                      Housing Rating ={" "}
+                    <ListGroup.Item
+                      className={this.scoreColor(
+                        this.props.job.CityData.categories[0].score_out_of_10
+                      )}
+                    >
+                      Housing Cost Rating ={" "}
                       {Math.round(
                         this.props.job.CityData.categories[0].score_out_of_10
                       )}
                     </ListGroup.Item>
-                    <ListGroup.Item>
+                    <ListGroup.Item
+                      className={this.scoreColor(
+                        this.props.job.CityData.categories[1].score_out_of_10
+                      )}
+                    >
                       Cost of Living Rating ={" "}
                       {Math.round(
                         this.props.job.CityData.categories[1].score_out_of_10
                       )}
                     </ListGroup.Item>
-                    <ListGroup.Item>
+                    <ListGroup.Item
+                      className={this.scoreColor(
+                        this.props.job.CityData.categories[8].score_out_of_10
+                      )}
+                    >
                       Health Care Rating ={" "}
                       {Math.round(
                         this.props.job.CityData.categories[8].score_out_of_10
                       )}
                     </ListGroup.Item>
-                    <ListGroup.Item>
+                    <ListGroup.Item
+                      className={this.scoreColor(
+                        this.props.job.CityData.categories[16].score_out_of_10
+                      )}
+                    >
                       Nature Vibes ={" "}
                       {Math.round(
                         this.props.job.CityData.categories[16].score_out_of_10
                       )}
                     </ListGroup.Item>
-                    <ListGroup.Item>
+                    <ListGroup.Item
+                      className={this.scoreColor(
+                        this.props.job.CityData.categories[14].score_out_of_10
+                      )}
+                    >
                       Leisure and Culture ={" "}
                       {Math.round(
                         this.props.job.CityData.categories[14].score_out_of_10
